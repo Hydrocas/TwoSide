d7c5550647427175e6234c9ce9915cffb1771419
HugoTeyssierFixe <h.teyssier@student.isartdigital.com>
2021-06-26 16:53:09 +0200

    setup scritps struture

---
+++ FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private const int MOUSE_BUTTON = 0;
+    private Camera mainCamera;
+    private bool isInit;
+
+    public void Init(Camera mainCamera)
+    {
+        isInit = true;
+        this.mainCamera = mainCamera;
+    }
+
+    private void Update()
+    {
+        if (!isInit || !Input.GetMouseButtonUp(MOUSE_BUTTON)) return;
+
+        Ray inputRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(inputRay.origin, inputRay.direction * 100, Color.red, 1000);
+    }
+}
---
+++ FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Garden garden = null;
+    [SerializeField] private Camera mainCamera = null;
+
+    private DayCycleManager dayCycleManager;
+    private Inventory inventory;
+    private Controller controller;
+    private RootManager rootManager;
+    private FlowerManager flowerManager;
+
+    private void Awake()
+    {
+        dayCycleManager = GetComponent<DayCycleManager>();
+        inventory = GetComponent<Inventory>();
+        controller = GetComponent<Controller>();
+        rootManager = GetComponent<RootManager>();
+        flowerManager = GetComponent<FlowerManager>();
+
+        controller.Init(mainCamera);
+    }
+}
