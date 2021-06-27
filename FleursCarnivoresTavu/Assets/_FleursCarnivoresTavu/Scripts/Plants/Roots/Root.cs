d669b6a1f1f84e98ef19c72e6de07843e12e78c0
HugoTeyssierFixe <h.teyssier@student.isartdigital.com>
2021-06-27 23:44:51 +0200

    root spawn

--- FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/Plants/Roots/Root.cs
+++ FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/Plants/Roots/Root.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class Root : Plant
 {
-    
+    [SerializeField] private float[] scaleGrowthLevel;
+    private Flower linkedFlower;
+
+    public void Init(Flower linkedFlower)
+    {
+        this.linkedFlower = linkedFlower;
+        Grow();
+    }
+
+    public override bool Grow()
+    {
+        if(!base.Grow()) return false;
+
+        transform.localScale = Vector3.one * scaleGrowthLevel[growthLevel];
+        linkedFlower.Grow();
+
+        return true;
+    }
 }
--- FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/Plants/SeedData.cs
+++ FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/Plants/SeedData.cs
@@ -6,4 +6,5 @@ public class SeedData : ScriptableObject
     public CharacterType type = default;
     public Sprite sprite = null;
     public Flower flower = null;
+    public Root root = null;
 }
