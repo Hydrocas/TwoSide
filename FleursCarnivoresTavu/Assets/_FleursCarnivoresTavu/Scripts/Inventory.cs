fd7e07cfdb0637fba0631b5f64d80a717dfd04dd
Shylin-Lee <romain.rs.sautron@gmail.com>
2021-06-27 23:06:22 +0200

    Revert "Merge branch 'UX' into main"
    This reverts commit c813ec703c31b0852a882d5dc375a51492defa70, reversing
    changes made to 221e14aee3aca3e6c3741467d4eea26ad0129b3e.

--- FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/Inventory.cs
+++ FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/Inventory.cs
@@ -5,25 +5,15 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     [SerializeField] private SeedData[] seedData;
-    [SerializeField] private BugData[] bugData;
 
     private int seedIndex;
-    private int bugIndex;
 
     public void SetCurrentSeed(int currentSeed)
     {
         seedIndex = currentSeed;
-        //Debug.Log(seedIndex);
+        Debug.Log(seedIndex);
     }
-
-    public void SetCurrentBug(int currentBug)
-    {
-        bugIndex = currentBug;
-        
-    }
-
     public SeedData CurrentSeed => seedData[seedIndex];
-    public BugData CurrentBug => bugData[bugIndex];
 
     public GameObject SelectedFlower()
     {
