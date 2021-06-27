504c8b8df4114bb1b258202b97a2c602c291003d
Shylin-Lee <romain.rs.sautron@gmail.com>
2021-06-27 16:09:14 +0200

    Update DayCycleManager.cs
    *day night cycle with timer

--- FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/DayCycleManager.cs
+++ FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/DayCycleManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class DayCycleManager : MonoBehaviour
 {
-    public float dayDuration = 0f;
-    public float nightDuration = 0f;
-    float _timer = 0;
+    public float dayDuration = 10f;
+    public float nightDuration = 5f;
+    float _timer = 0f;
+    [HideInInspector]
     public bool isNight = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +20,26 @@ public class DayCycleManager : MonoBehaviour
     void Update()
     {
         _timer += Time.deltaTime;
+        if(_timer>= dayDuration && !isNight)
+        {
+            DoNight();
+        }
+        if(_timer>= dayDuration + nightDuration && isNight)
+        {
+            DoDay();
+            _timer = 0f;
+        }
     }
 
     void DoNight()
     {
         isNight = true;
+        //Debug.Log(isNight);
     }
 
     void DoDay()
     {
         isNight = false;
+        //Debug.Log(isNight);
     }
 }
