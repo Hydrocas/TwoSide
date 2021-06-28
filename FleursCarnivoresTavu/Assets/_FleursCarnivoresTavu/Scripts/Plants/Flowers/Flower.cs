ce5b8a1728863b4f89e39571a3548cf7fcecd3d9
HugoTeyssierFixe <h.teyssier@student.isartdigital.com>
2021-06-28 13:49:33 +0200

    animation flower

--- FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/Plants/Flowers/Flower.cs
+++ FleursCarnivoresTavu/Assets/_FleursCarnivoresTavu/Scripts/Plants/Flowers/Flower.cs
@@ -6,26 +6,32 @@ public class Flower : Plant
     [SerializeField] private BugStack givenBugs = default;
     [SerializeField] private SeedData givenSeed = default;
     [SerializeField] private int spawnBugLevel = 2;
-    [SerializeField] private Sprite[] growthSprites = null;
+    //[SerializeField] private Sprite[] growthSprites = null;
 
     public BugStack GivenBugs => givenBugs;
 
     public event Action<Flower> OnGiveBugs;
+    private Animator animator;
 
     private bool hasFruit;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     public override bool Grow()
     {
         if(!base.Grow()) return false;
 
-        spriteRenderer.sprite = growthSprites[growthLevel];
+        animator.SetInteger("growthLevel", growthLevel);
 
         return true;
     }
 
     public void Init()
     {
-
+        
     }
 
     public void SetDayMode()
